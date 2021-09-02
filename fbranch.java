61b1a8086dfd5803844d51a9ad95942059c0596c
dahshatJD <89901056+dahshatJD@users.noreply.github.com>
2021-09-02 12:06:57 +0530

    Update fbranch.java

--- fbranch.java
+++ fbranch.java
@@ -2,8 +2,9 @@ public class fbranch {
     
     public static void name() {
         int j= 4;
-        int k= 10;
+        int k= 11;
         System.out.println(j*k);
+        
         
     }
     
