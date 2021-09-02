76becc2ca82c45be40008f30c540dc04e66ead75
jayesh <dhanawadejayesh2001@gmail.com>
2021-09-02 11:32:54 +0530

    man.java commit

---
+++ man.java
@@ -0,0 +1,12 @@
+public class man {
+
+    
+}
+ public static void main(String[] args) {  
+        //defining an 'age' variable  gir
+        int age=65;  
+        //checking the age  
+        if(age>26){  
+            System.out.print("Age is greater than 18");  
+        }  
+    }
